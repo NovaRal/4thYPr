f221396eef3ca9c091ae013f3cbc2092fc0d89c5
NovaRal <novaxred@gmail.com>
2023-11-16 01:27:17 +0530

    Added login end point

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,8 +1,9 @@
 import express  from "express";
-import { getAllUser, signUp } from "../controllers/user-controller";
+import { getAllUser, login, signUp } from "../controllers/user-controller";
 
 const router = express.Router();
 
 router.get("/", getAllUser);
-router.post("/signUp",signUp);
+router.post("/signup",signUp);
+router.post("/login",login);
 export default router;
